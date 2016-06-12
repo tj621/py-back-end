7a6ea852d589b1ab9516722acbbc9b4f399d5684
cbwuyanzu <277850402@qq.com>
2016-06-13 00:37:33 +0800

    june 13

--- indoor.py
+++ indoor.py
@@ -1,34 +1,32 @@
 class Indoor:
     '''the object of indoor climate'''
 
-    temperature = 0
-    humidity = 0
-    radiation = 0
-    co2 = 0
-
     def __init__(self):
-        pass
+        self.__temperature = 0
+        self.__humidity = 0
+        self.__radiation = 0
+        self.__co2 = 0
 
     def set_temperature(self, temperature):
-        self.temperature = temperature
+        self.__temperature = temperature
 
     def set_humidity(self, hum):
-        self.humidity = hum
+        self.__humidity = hum
 
     def set_radiation(self, rad):
-        self.radiation = rad
+        self.__radiation = rad
 
     def set_co2(self, co2):
-        self.co2 = co2
+        self.__co2 = co2
 
     def get_temperature(self):
-        return self.temperature
+        return self.__temperature
 
     def get_humidity(self):
-        return self.humidity
+        return self.__humidity
 
     def get_radiation(self):
-        return self.radiation
+        return self.__radiation
 
     def get_co2(self):
-        return self.co2
+        return self.__co2
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from indoor import Indoor
+from control import Control
 
-from flask import Flask
+from flask import Flask, request
 from flask import abort
 from flask import redirect
 
@@ -9,18 +10,19 @@ import time
 app = Flask(__name__)
 
 node0 = Indoor()
-print "temperature", node0.temperature
-print "humidity", node0.humidity
-print "radiation", node0.radiation
-print "co2", node0.co2
+print "temperature", node0.get_temperature()
+print "humidity", node0.get_humidity()
+print "radiation", node0.get_radiation()
+print "co2", node0.get_co2()
 node0.set_temperature(20.0)
 node0.set_humidity(80.0)
 node0.set_radiation(8000)
 node0.set_co2(500)
-print "temperature", node0.temperature
-print "humidity", node0.humidity
-print "radiation", node0.radiation
-print "co2", node0.co2
+print "temperature", node0.get_temperature()
+print "humidity", node0.get_humidity()
+print "radiation", node0.get_radiation()
+print "co2", node0.get_co2()
+c = Control()
 
 
 def get_now_time():
@@ -37,10 +39,39 @@ def index():
 
 @app.route('/indoor')
 def get_indoor():
-    return '''{"indoor": {"node_0": {"updatetime": "%s",
+    return '''{"indoor": {"node_0": {"update_time": "%s",
     "temperature": "%s","relative_humidity": "%s",
     "radiation": "%s","co2": "%s"}}}''' \
-           % (get_now_time(), node0.temperature, node0.humidity, node0.radiation, node0.co2)
+           % (get_now_time(), node0.get_temperature(), node0.get_humidity(), node0.get_radiation(), node0.get_co2())
+
+
+@app.route('/control', methods=['GET', 'POST'])
+def control():
+    if request.method == 'POST':
+        pass
+    else:
+        return '''{
+        "actuator": {
+        "update_time":"%s",
+        "tri_state": {"roof_vent_south": "%s",
+            "roof_vent_north": "%s",
+            "side_vent": "%s","shade_screen_out": "%s",
+            "shade_screen_in": "%s","thermal_screen": "%s"},
+        "bi_state": {
+            "cooling_pad": "%s","fogging": "%s",
+            "heating": "%s","co2": "%s",
+            "lighting_1": "%s","lighting_2": "%s",
+            "irrigation": "%s"}}}''' \
+               % (get_now_time(), c.get_roof_vent_south(), c.get_roof_vent_north(),
+                  c.get_side_vent(), c.get_shade_screen_out(), c.get_shade_screen_in(),
+                  c.get_thermal_screen(), c.get_cooling_pad(), c.get_fogging(), c.get_heating(),
+                  c.get_co2(), c.get_lighting_1(), c.get_lighting_2(), c.get_irrigation())
+
+
+@app.route('/hi')
+def change():
+    node0.set_temperature(30.0)
+    return '<h1>set temp from 20 to 30</h1>'
 
 
 if __name__ == '__main__':
